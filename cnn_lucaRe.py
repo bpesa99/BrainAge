40e0aec555b4f26ce7ca1ac31826939c86e4495c
bru <bruno.pesa99@gmail.com>
2024-11-13 11:19:52 +0100

    Kommentar

--- cnn_lucaRe.py
+++ cnn_lucaRe.py
@@ -72,51 +72,55 @@ X_test_data = np.expand_dims(X_test_data, axis=-1) # -1 fügt ans Ende eine Dime
 X_train_data = X_train_data / np.max(X_train_data)
 X_test_data = X_test_data / np.max(X_test_data)
 
-def create_callbacks():
-    # Learning Rate Scheduler
-    lr_scheduler = tf.keras.callbacks.LearningRateScheduler(
-        lambda epoch: 1e-5 * 10 ** (epoch / 20)
-    )
-
-    # Early Stopping
-    early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor='val_loss',
-        patience=5,
-        restore_best_weights=True
-    )
-
-    return [lr_scheduler, early_stopping]
-
 # 7. CNN Modell definieren
 def create_cnn_model(input_shape):
     model = tf.keras.models.Sequential([
         tf.keras.Input(shape=input_shape),
-        #Je tiefer die Schichten der Convolutional Layer, umso größer ist i.d.R. die Anzahl an Filtern.
-        #Am Anfang werden meistens einfache Merkmale(Kanten, Ecken, Texturen) erkannt, weshalb auch weniger Filter ausreichen.
-        #In den tieferen Schichten versucht das Modell komplexere Merkmale(z.B. Kombinationen aus Kanten und Texturen)
-        #zu erkennen, weshalb man hier auch mehr Filter braucht.
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.Conv2D(8, (3, 3), activation='relu'),
+        # Je tiefer die Schichten der Convolutional Layer, umso größer ist i.d.R. die Anzahl an Filtern (z.B. 32 bis 512 Filter)
+        # Am Anfang werden meistens einfache Merkmale(Kanten, Ecken, Texturen) erkannt, weshalb auch weniger Filter ausreichen.
+        # In den tieferen Schichten versucht das Modell komplexere Merkmale(z.B. Kombinationen aus Kanten und Texturen)
+        # zu erkennen, weshalb man hier auch mehr Filter braucht.
+        # Wenn die Anzahl an Convolutional Layern zu groß ist, dann tritt ein negative dimension error auf
+        tf.keras.layers.BatchNormalization(),
+        # Die Batch-Normalisierung normalisiert die Ausgaben eines Layers auf Mini-Batch-Ebene.
+        # Für jeden Mini-Batch(Teil der Daten, der gleichzeitig durch das Netzwerk geht) werden die Verteilungen der Werte
+        # so angepasst, dass jede Ausgabe nach der Normalisierung einen Mittelwert von 0 und eine Standardabweichung von 1 hat,
+        # was das Training vereinfachen un die Konvergenz verbessern soll.
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        # Max-Pooling reduziert die räumliche Dimension der Daten, indem es die wichtigsten Merkmale extrahiert und die Anzahl der zu bearbeitenden Informatioonen
+        # reduziert, was die Berechnungen vereinfacht und die Effizienz erhöht.
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPooling2D((2, 2)),
 
         tf.keras.layers.Flatten(),
-
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(512, activation='relu'),
-        #tf.keras.layers.Dropout(0.5), #Dropout zur Vermeidung von Überanpassung
-        tf.keras.layers.Dense(1, activation='linear')  # Regression für Altersvorhersage
+        #Die Funktion Flatten() wandelt mehrdimensionale Daten in einen eindimensionalen Vektor um,
+        #damit die Hidden Layer die Daten der Convolutioonal Layer bearbeiten können.
+        tf.keras.layers.Dense(128, activation='relu'),
+        # Die Hidden Layer berechnen eine gewichtete Summe, um nichtlineare Beziehungen abzubilden.
+        # Für komplexe Aufgaben können es zwischen 1024 und 4096 Neuronen oder mehr sein.
+        # Jedes Neuron in einer solchen Layer lernt eine bestimmte Transformation der Daten, damit das Netzwerk koomplexe Muster erkennen kann.
+        # Umso mehr Hidden Layer man hat, umso leichter kann das Netzwerk komplexe Abhängigkeiten in den Daten erfassen.
+        # Die units bestimmen die Anzahl an Neuronen/Knoten in einer Hidden Layer. Mehr Neuronen können dem Layer ermöglichen,
+        # mehr und komplexere Merkmale zu lernen, was dann aber auch mehr Rechenzeit benötigt.
+        # Zu viele Neuronen können jedoch zu Overfitting führen.
+        # Overfitting:   - Das Modell ist zu komplex, z.B. lernt es feine Details, die nur in den Trainingsdaten vorkommen
+        #               - Das Modell wurde zu lange trainiert und hat sich zu sehr den Trainingsdaten angepasst
+        # Die Aktivierungsfunktion entscheidet, wie die Ausgabe jedes Neurons transformiert wird und ist notwendig, um Nichtlinearitäten abzubilden.
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear')
+        # Wir wollen nur das geschätzte Alter wissen, deshalb gibt es in der letzten Layer nur ein Neuron.
+        # Die Aktievierungsfunktion linear bzw. None gibt das Alter ohne Transformation aus.
     ])
     return model
 
@@ -127,18 +131,28 @@ model = create_cnn_model(input_shape)
 model.summary()
 
 # Modell kompilieren (Regression für Altersvorhersage)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+# Der Adam-Optimierer wird oft für tiefe neuronale Netzwerke eingesetzt, da er effizient und robust gegenüber Hyperparametereinstellungen ist.
+# Die learing_rate legt fest, wie groß die Schritte sind, die das Netzwerk bei jeder Aktualisierung der Gewichte macht, um den Verlust zu minimieren.
+# Ist die learing_rate zu groß kann es sein, dass das Modell divergiert statt zu konvergieren.
+# Und ist die learing_rate zu klein dauert es sehr lange bi sdas Modell konvergiert.
+# Für Adam wäre ein typischer Wert für die learning_rate 0.001
 model.compile(optimizer=optimizer, loss='mean_absolute_error', metrics=['mean_absolute_error'])
 
-history = model.fit(X_train_data, y_train_data, epochs=25, batch_size=246, validation_data=(X_test_data, y_test_data)) #, callbacks=create_callbacks())
+history = model.fit(X_train_data, y_train_data, epochs=5, batch_size=512, validation_data=(X_test_data, y_test_data))
+# In jeder Epoche wird das Modell auf den Trainingsdaten trainiert und die Gewichte werden angepasst.
+# Die batch_size legt fest, wie viele Daten gleichzeitig bearbeitet werden.
+# Ist die batch_size zu groß kann es zu Speichermangel führen und ist sie zu klein verlangsamt dies das Training.
+# Die validation_data dient dazu das Modell auf ungesehenen Daten zu evaluieren, um zu überwachen ob es z.B. zu Overfitting kommt.
 model.save('BrainAge.keras')
 #print(history.history)
-plt.plot(history.history['mean_absolute_error'], label='mean_absolute_error')
-plt.plot(history.history['val_mean_absolute_error'], label='val_mean_absolute_error')
-plt.xlabel('Epoch')
-plt.ylabel('Mean Absolute Error')
+#plt.plot(history.history['mean_absolute_error'], label='mean_absolute_error')
+#plt.plot(history.history['val_mean_absolute_error'], label='val_mean_absolute_error')
+#plt.xlabel('Epoch')
+#plt.ylabel('Mean Absolute Error')
 #plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
+#plt.legend(loc='lower right')
 
 # 9. Evaluierung des Modells auf Testdaten
 test_loss, test_acc = model.evaluate(X_test_data, y_test_data)
+#Hier wird das bereits trainierte Modell bewertet.
